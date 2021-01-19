0b0108c86132c1cd63c1f69bb99a02aac88611aa
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-01-19 09:42:44 -0700

    OrderForm correctly updates state on input

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -6,49 +6,73 @@ class OrderForm extends Component {
     this.props = props;
     this.state = {
       name: '',
-      ingredients: []
+      ingredients: [],
     };
   }
 
-
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
     this.clearInputs();
-  }
+  };
 
   clearInputs = () => {
-    this.setState({name: '', ingredients: []});
-  }
+    this.setState({ name: '', ingredients: [] });
+  };
+
+  handleNameChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleIngredientChange = (e) => {
+    e.preventDefault();
+    if (this.state.ingredients.includes(e.target.name)) return;
+    this.setState({ ingredients: [...this.state.ingredients, e.target.name] });
+  };
 
   render() {
-    const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
-    const ingredientButtons = possibleIngredients.map(ingredient => {
+    const possibleIngredients = [
+      'beans',
+      'steak',
+      'carnitas',
+      'sofritas',
+      'lettuce',
+      'queso fresco',
+      'pico de gallo',
+      'hot sauce',
+      'guacamole',
+      'jalapenos',
+      'cilantro',
+      'sour cream',
+    ];
+    const ingredientButtons = possibleIngredients.map((ingredient) => {
       return (
-        <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
+        <button
+          key={ingredient}
+          name={ingredient}
+          onClick={(e) => this.handleIngredientChange(e)}
+        >
           {ingredient}
         </button>
-      )
+      );
     });
 
     return (
       <form>
         <input
-          type='text'
-          placeholder='Name'
-          name='name'
+          type="text"
+          placeholder="Name"
+          name="name"
           value={this.state.name}
-          onChange={e => this.handleNameChange(e)}
+          onChange={(e) => this.handleNameChange(e)}
         />
 
-        { ingredientButtons }
+        {ingredientButtons}
 
-        <p>Order: { this.state.ingredients.join(', ') || 'Nothing selected' }</p>
+        <p>Order: {this.state.ingredients.join(', ') || 'Nothing selected'}</p>
 
-        <button onClick={e => this.handleSubmit(e)}>
-          Submit Order
-        </button>
+        <button onClick={(e) => this.handleSubmit(e)}>Submit Order</button>
       </form>
-    )
+    );
   }
 }
 
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -4,9 +4,9 @@ import './Orders.css';
 const Orders = (props) => {
   const orderEls = props.orders.map((order) => {
     return (
-      <div className="order">
+      <div className="order" key={order.id}>
         <h3>{order.name}</h3>
-        <ul className="ingredient-list" key={order.id}>
+        <ul className="ingredient-list">
           {order.ingredients.map((ingredient) => {
             return <li>{ingredient}</li>;
           })}
