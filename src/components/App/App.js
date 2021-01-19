745c8fa1cbf9fe37f0f3811aa50e7fedb525dc89
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-01-19 09:29:47 -0700

    Create state for App, update on Mount

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import { getOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
   constructor(props) {
     super();
+    this.state = {
+      orders: [],
+    };
   }
 
   componentDidMount() {
     getOrders()
-      .catch(err => console.error('Error fetching:', err));
+      .then((data) => {
+        this.setState({ orders: data.orders });
+      })
+      .catch((err) => console.error('Error fetching:', err));
   }
 
   render() {
     return (
       <main className="App">
-        <header>
-          <h1>Burrito Builder</h1>
-          <OrderForm />
-        </header>
+        {this.state.orders.length && (
+          <div>
+            <header>
+              <h1>Burrito Builder</h1>
+              <OrderForm />
+            </header>
 
-        <Orders orders={this.state.orders}/>
+            <Orders orders={this.state.orders} />
+          </div>
+        )}
       </main>
     );
   }
 }
-
 
 export default App;
